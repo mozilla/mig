91f702ba2d8e11a22e333e166060a9aa29f36b04
Julien Vehent <julien@linuxwall.info>
2014-02-24 09:00:43 -0800

    [doc] move development tools

--- inotify_make.sh
+++ inotify_make.sh
@@ -3,22 +3,11 @@
 # watch the MIG source code directory and rebuild the components
 # when a file is saved.
 
-if [[ -z $1 || ! -d "$1" ]]; then
-    cat <<EOF
-usage: $0 ./src/mig
-
-$0 watches the content of the MIG source directory and
-rebuilds components when a source code file is modified.
-It must be ran from the directory of the MIG Makefile.
-EOF
-    exit 1
-fi
-
-echo "starting inotify listener on $1"
+echo "starting inotify listener on ./src/mig"
 # feed the inotify events into a while loop that creates
 # the variables 'date' 'time' 'dir' 'file' and 'event'
 inotifywait -mr --timefmt '%d/%m/%y %H:%M' --format '%T %w %f %e' \
--e modify $1 \
+-e modify ./src/mig \
 | while read date time dir file event
 do
     if [[ "$file" =~ \.go$ && "$dir" =~ src\/mig ]]; then
