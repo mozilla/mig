7ba649417e0450fc054420d7eb5073a5424f0562
Julien Vehent <julien@linuxwall.info>
2015-03-15 11:24:06 -0400

    [medium] ping module improvements: structs, logic and doc

--- src/mig/modules/ping/ping.go
+++ src/mig/modules/ping/ping.go
@@ -2,7 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-// Contributor: Sushant Dinesh sushant.dinesh94@gmail.com [:sushant94]
+// Contributors: Sushant Dinesh sushant.dinesh94@gmail.com [:sushant94]
+//               Julien Vehent jvehent@mozilla.com [:ulfr]
 
 // ping module is used to check the connection between an endpoint
 // and a destination host.
@@ -40,37 +41,103 @@ type params struct {
 	Protocol        string  `json:"protocol"`                  // icmp, tcp, udp
 	Count           float64 `json:"count,omitempty"`           // Number of tests
 	Timeout         float64 `json:"timeout,omitempty"`         // Timeout for individual test. defaults to 5s.
+	ipDest          string
 }
 
-type elements struct {
-	MsLatencies  []float64 `json:"ms_latencies"` // response latency in milliseconds: 9999999 indicates timeout, -1 indicates unreachable, 0 general error.
-	Reachable    bool      `json:"reachable"`
+type pingresults struct {
+	Latencies    []float64 `json:"latencies"`    // response latency in milliseconds: 9999999 indicates timeout, -1 indicates unreachable, 0 general error.
+	Protocol     string    `json:"protocol"`     // icmp, tcp, udp
 	ResolvedHost string    `json:"resolvedhost"` // Information about the ip:port being pinged
+	Failures     []string  `json:"failures"`     // ping failures, soft errors
 }
 
-// Global variable latencies.
-var latencies elements
+const (
+	E_ConnRefused = "connection refused"
+	E_Timeout     = "connection timed out"
+)
+
+func (r Runner) Run(Args []byte) string {
+	var (
+		err error
+		pr  pingresults
+	)
+	err = json.Unmarshal(Args, &r.Parameters)
+	if err != nil {
+		r.Results.Errors = append(r.Results.Errors, fmt.Sprintf("%v", err))
+		return r.buildResults()
+	}
+	err = r.ValidateParameters()
+	if err != nil {
+		r.Results.Errors = append(r.Results.Errors, fmt.Sprintf("%v", err))
+		return r.buildResults()
+	}
+
+	pr.ResolvedHost = r.Parameters.Destination
+	if r.Parameters.Protocol == "udp" || r.Parameters.Protocol == "tcp" {
+		pr.ResolvedHost += fmt.Sprintf(":%.0f", r.Parameters.DestinationPort)
+	}
+	pr.ResolvedHost += " (" + r.Parameters.ipDest + ")"
+	pr.Protocol = r.Parameters.Protocol
+	for i := 0; i < int(r.Parameters.Count); i += 1 {
+		var err error
+		// startTime for calculating the latency/RTT
+		startTime := time.Now()
+
+		switch r.Parameters.Protocol {
+		case "icmp":
+			err = r.pingIcmp()
+		case "tcp":
+			err = r.pingTcp()
+		case "udp":
+			err = r.pingUdp()
+		}
+		// store the time elapsed before processing potential errors
+		latency := time.Since(startTime).Seconds() * 1000
+
+		// evaluate potential ping failures
+		if err != nil {
+			switch err.Error() {
+			case E_Timeout:
+				latency = 9999999
+			case E_ConnRefused:
+				latency = -1
+			default:
+				pr.Failures = append(pr.Failures, fmt.Sprintf("ping #%d failed with error: %v", i+1, err))
+				latency = 0
+			}
+		}
+
+		switch latency {
+		case -1, 0:
+			// do nothing
+		case 9999999:
+			// For udp, a timeout indicates that the port *maybe* open.
+			if r.Parameters.Protocol == "udp" {
+				r.Results.FoundAnything = true
+			}
+		default:
+			r.Results.FoundAnything = true
+		}
+		pr.Latencies = append(pr.Latencies, latency)
+	}
+	r.Results.Elements = pr
+	return r.buildResults()
+}
 
 func (r *Runner) ValidateParameters() (err error) {
-	// Check if Protocol is a valid one that we support with this module
-	if r.Parameters.Protocol != "icmp" &&
-		r.Parameters.Protocol != "udp" &&
-		r.Parameters.Protocol != "tcp" {
-		return fmt.Errorf("Unsupported protocol requested")
+	// check if Protocol is a valid one that we support with this module
+	switch r.Parameters.Protocol {
+	case "icmp", "udp", "tcp":
+		break
+	default:
+		return fmt.Errorf("%s is not a supported ping protocol", r.Parameters.Protocol)
 	}
-
-	// Check if protocol is icmp and a destinationport is specified
-	if r.Parameters.Protocol == "icmp" && r.Parameters.DestinationPort != 0 {
-		return fmt.Errorf("icmp does not take a destinationport parameter")
-	} else if r.Parameters.Protocol != "icmp" && r.Parameters.DestinationPort == 0 {
-		r.Parameters.DestinationPort = 80
+	// tcp and udp pings must have a destination port
+	if r.Parameters.Protocol != "icmp" && (r.Parameters.DestinationPort < 0 || r.Parameters.DestinationPort > 65535) {
+		return fmt.Errorf("%s ping requires a valid destination port between 0 and 65535, got %.0f",
+			r.Parameters.Protocol, r.Parameters.DestinationPort)
 	}
-
-	// Check if port number is in valid range
-	if !(r.Parameters.DestinationPort >= 0 && r.Parameters.DestinationPort <= 65535) {
-		return fmt.Errorf("Invalid DestinationPort: %v", r.Parameters.DestinationPort)
-	}
-
+	// if the destination is a FQDN, resolve it and take the first IP returned as the dest
 	ips, err := net.LookupHost(r.Parameters.Destination)
 	ip := ""
 	// Get ip based on destination.
@@ -84,26 +151,29 @@ func (r *Runner) ValidateParameters() (err error) {
 		ip = ips[0]
 	}
 
+	// check the format of the destination IP
 	ip_parsed := net.ParseIP(ip)
-	// If ParseIP failed, then ip is not a valid IP.
 	if ip_parsed == nil {
-		return fmt.Errorf("Invalid Destination: %v", ip)
+		return fmt.Errorf("destination IP is invalid: %v", ip)
 	}
+	r.Parameters.ipDest = ip
 
+	// if timeout is not set, default to 5 seconds
 	if r.Parameters.Timeout == 0.0 {
 		r.Parameters.Timeout = 5.0
 	}
 
-	// Default count to 3 if it is 0
+	// if count of pings is not set, default to 3
 	if r.Parameters.Count == 0.0 {
 		r.Parameters.Count = 3
 	}
 
-	r.Parameters.Destination = ip
 	return
 }
 
-func (r Runner) Ping() (err error) {
+// pingIcmp performs a ping to a destination. It select between ipv4 or ipv6 ping based
+// on the format of the destination ip.
+func (r Runner) pingIcmp() (err error) {
 	var (
 		icmpType icmp.Type
 		network  string
@@ -152,9 +222,12 @@ func (r Runner) Ping() (err error) {
 	rb := make([]byte, 1500)
 
 	if _, err := c.Read(rb); err != nil {
-		// If Timeout, we return "Timeout"
+		// If connection timed out, we return E_Timeout
 		if e := err.(*net.OpError).Timeout(); e {
-			return fmt.Errorf("Timeout")
+			return fmt.Errorf(E_Timeout)
+		}
+		if strings.Contains(err.Error(), "connection refused") {
+			return fmt.Errorf(E_ConnRefused)
 		}
 		return fmt.Errorf("Conn.Read failed: %v", err)
 	}
@@ -167,30 +240,35 @@ func (r Runner) Ping() (err error) {
 	return
 }
 
-func (r Runner) tcpPing() (err error) {
-	// Make it ip:port format
-	destination := r.Parameters.Destination + ":" + fmt.Sprintf("%d", int(r.Parameters.DestinationPort))
-
-	// If dial Timeout it means that the handshake could not be completed and the port may not be open.
-	if _, err := net.DialTimeout("tcp", destination, time.Duration(r.Parameters.Timeout)*time.Second); err != nil {
-		// If Timeout, we return "Timeout"
+// pingTcp performs a straighfoward connection attempt on a destination ip:port and returns
+// an error if the attempt failed
+func (r Runner) pingTcp() (err error) {
+	conn, err := net.DialTimeout("tcp",
+		fmt.Sprintf("%s:%d", r.Parameters.Destination, int(r.Parameters.DestinationPort)),
+		time.Duration(r.Parameters.Timeout)*time.Second)
+	defer conn.Close()
+	if err != nil {
+		// If connection timed out, we return E_Timeout
 		if e := err.(*net.OpError).Timeout(); e {
-			return fmt.Errorf("Timeout")
+			return fmt.Errorf(E_Timeout)
+		}
+		if strings.Contains(err.Error(), "connection refused") {
+			return fmt.Errorf(E_ConnRefused)
 		}
 		return fmt.Errorf("Dial Error: %v", err)
 	}
 	return
 }
 
-// Open UDP ports are hard to determine.
-// If there is a Timeout on Read it means that the port *maybe* open,
-// however a connection refused either means that the port is filtered or closed.
-func (r Runner) udpPing() (err error) {
+// pingUdp sends a UDP packet to a destination ip:port to determine if it is open or closed.
+// Because UDP does not reply to connection requests, a lack of response may indicate that the
+// port is open, or that the packet got dropped. We chose to be optimistic and treat lack of
+// response (connection timeout) as an open port.
+func (r Runner) pingUdp() (err error) {
 	// Make it ip:port format
 	destination := r.Parameters.Destination + ":" + fmt.Sprintf("%d", int(r.Parameters.DestinationPort))
 
 	c, err := net.Dial("udp", destination)
-
 	if err != nil {
 		return fmt.Errorf("Dial Error: %v", err)
 	}
@@ -202,82 +280,22 @@ func (r Runner) udpPing() (err error) {
 	rb := make([]byte, 1500)
 
 	if _, err := c.Read(rb); err != nil {
-		// If Timeout, we return "Timeout"
+		// If connection timed out, we return E_Timeout
 		if e := err.(*net.OpError).Timeout(); e {
-			return fmt.Errorf("Timeout")
+			return fmt.Errorf(E_Timeout)
 		}
-
+		if strings.Contains(err.Error(), "connection refused") {
+			return fmt.Errorf(E_ConnRefused)
+		}
 		return fmt.Errorf("Read Error: %v", err.Error())
 	}
-
 	return nil
 }
 
-func (r Runner) Run(Args []byte) string {
-
-	err := json.Unmarshal(Args, &r.Parameters)
-	if err != nil {
-		r.Results.Errors = append(r.Results.Errors, fmt.Sprintf("%v", err))
-		return r.buildResults()
-	}
-
-	err = r.ValidateParameters()
-	if err != nil {
-		r.Results.Errors = append(r.Results.Errors, fmt.Sprintf("%v", err))
-		return r.buildResults()
-	}
-
-	latencies.Reachable = false
-	latencies.ResolvedHost = fmt.Sprintf("%s:%v", r.Parameters.Destination, r.Parameters.DestinationPort)
-
-	for i := 0; i < int(r.Parameters.Count); i += 1 {
-		var err error
-		// startTime for calculating the latency/RTT
-		startTime := time.Now()
-
-		switch r.Parameters.Protocol {
-		case "icmp":
-			err = r.Ping()
-		case "tcp":
-			err = r.tcpPing()
-		case "udp":
-			err = r.udpPing()
-		}
-
-		latency := time.Since(startTime).Seconds() * 1000
-
-		if err != nil {
-			if err.Error() == "Timeout" {
-				latency = 9999999
-			} else if strings.Contains(err.Error(), "connection refused") {
-				latency = -1
-			} else {
-				r.Results.Errors = append(r.Results.Errors, fmt.Sprintf("Fail on test#%d (%v)", i+1, err))
-				latency = 0
-			}
-		}
-
-		switch latency {
-		case -1, 0:
-			// do nothing
-		case 9999999:
-			// For udp, a timeout indicates that the port *maybe* open.
-			if r.Parameters.Protocol == "udp" {
-				latencies.Reachable = true
-			}
-		default:
-			latencies.Reachable = true
-		}
-		latencies.MsLatencies = append(latencies.MsLatencies, latency)
-	}
-
-	r.Results.Success = true
-	return r.buildResults()
-}
-
 func (r Runner) buildResults() string {
-	r.Results.FoundAnything = latencies.Reachable
-	r.Results.Elements = latencies
+	if len(r.Results.Errors) == 0 {
+		r.Results.Success = true
+	}
 	jsonOutput, err := json.Marshal(r.Results)
 	if err != nil {
 		panic(err)
@@ -292,38 +310,55 @@ func (r Runner) PrintResults(rawResults []byte, foundOnly bool) (prints []string
 		}
 	}()
 
-	var modres mig.ModuleResult
-	err = json.Unmarshal(rawResults, modres)
+	var results mig.ModuleResult
+	err = json.Unmarshal(rawResults, &results)
 	if err != nil {
 		panic(err)
 	}
-	buf, err := json.Marshal(modres)
+	buf, err := json.Marshal(results.Elements)
 	if err != nil {
 		panic(err)
 	}
-
-	var elms elements
-	err = json.Unmarshal(buf, elms)
+	var pr pingresults
+	err = json.Unmarshal(buf, &pr)
 	if err != nil {
 		panic(err)
 	}
-
-	resStr := fmt.Sprintf("Pinged host %s\nResults:\n", elms.ResolvedHost)
-
-	if elms.Reachable == false {
-		resStr = resStr + "  Host Unreachable\n"
-		prints = append(prints, resStr)
+	if results.FoundAnything {
+		prints = append(prints,
+			fmt.Sprintf("%s ping of %s succeeded. Target is reachable.",
+				pr.Protocol,
+				pr.ResolvedHost,
+			),
+		)
+	}
+	// if we don't care about results where the target was not reachable, stop here
+	if foundOnly {
 		return
 	}
-
-	resStr = " Latencies: "
-	for i := range elms.MsLatencies {
-		resStr += fmt.Sprintf("%v ", elms.MsLatencies[i])
+	if !results.FoundAnything {
+		prints = append(prints,
+			fmt.Sprintf("%s ping of %s failed. Target is no reachable.",
+				pr.Protocol,
+				pr.ResolvedHost,
+			),
+		)
 	}
-	resStr += "\n"
-
-	resStr = resStr + "  Host Reachable\n"
-	prints = append(prints, resStr)
-
+	for i, lat := range pr.Latencies {
+		switch lat {
+		case -1:
+			prints = append(prints, fmt.Sprintf("ping #%d failed, target was unreachable", i+1))
+		case 0:
+			prints = append(prints, fmt.Sprintf("ping #%d failed, reason unknown", i+1))
+		case 9999999:
+			if pr.Protocol == "udp" {
+				prints = append(prints, fmt.Sprintf("ping #%d may have succeeded (no udp response)", i+1))
+			} else {
+				prints = append(prints, fmt.Sprintf("ping #%d failed, connection timed out", i+1))
+			}
+		default:
+			prints = append(prints, fmt.Sprintf("ping #%d succeeded in %.0fms", i+1, lat))
+		}
+	}
 	return
 }
