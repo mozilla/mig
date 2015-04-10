822fe1f1ef207715a2a89ea7c86e74d5f278f600
Aaron Meihm <ameihm@mozilla.com>
2015-04-10 15:14:08 -0500

    [minor] basic initial structure for migoval module

---
+++ src/mig/modules/migoval/migoval.go
@@ -0,0 +1,40 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Contributor: Aaron Meihm ameihm@mozilla.com [:alm]
+package migoval
+
+import (
+	"github.com/ameihm0912/mozoval/go/src/oval"
+	"mig"
+)
+
+func init() {
+	mig.RegisterModule("migoval", func() interface{} {
+		return new(Runner)
+	})
+}
+
+type Runner struct {
+	Parameters Parameters
+}
+
+func (r Runner) ValidateParameters() (err error) {
+	return
+}
+
+type Results struct {
+}
+
+type Parameters struct {
+	modePkgList bool `json:"pkglistmode"`
+}
+
+func newParameters() *Parameters {
+	return &Parameters{}
+}
+
+func migovalInitialize() {
+	oval.Init()
+}
