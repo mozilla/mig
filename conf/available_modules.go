ede3858edbb94fcee44f30b28c488cd13207eff5
Julien Vehent <julien@linuxwall.info>
2015-08-26 17:14:37 -0400

    [minor/bug] disable memory module to prevent macos build from breaking

--- conf/available_modules.go
+++ conf/available_modules.go
@@ -8,7 +8,8 @@ package main
 import (
 	_ "mig.ninja/mig/modules/agentdestroy"
 	_ "mig.ninja/mig/modules/file"
-	_ "mig.ninja/mig/modules/memory"
+	// https://github.com/mozilla/masche/issues/31
+	//_ "mig.ninja/mig/modules/memory"
 	_ "mig.ninja/mig/modules/netstat"
 	_ "mig.ninja/mig/modules/ping"
 	_ "mig.ninja/mig/modules/pkg"
