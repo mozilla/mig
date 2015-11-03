mozilla/mig

commit e2e687f9a1864a10199da06dd42c02d00aa1e36d

[minor] Add travis-ci test for running agent from file.
I'm applying the changes suggested in PR #139 by @jvehent; these make
runModuleDurectly to not use stdin, instead only use the given
parameters.

Now travis-ci should test mig-agent after installing to see that it can
run an action individually.

mig-agent/agent.go
@@ -179,11 +179,7 @@ func executeAction(action mig.Action, prettyPrint bool) (cmd mig.Command, err er
 
 	// launch each operation consecutively
 	for _, op := range action.Operations {
-		args, err := json.Marshal(op.Parameters)
-		if err != nil {
-			panic(err)
-		}
-		out := runModuleDirectly(op.Module, args, prettyPrint)
+		out := runModuleDirectly(op.Module, op.Parameters, prettyPrint)
 		var res modules.Result
 		err = json.Unmarshal([]byte(out), &res)
 		if err != nil {
@@ -195,13 +191,17 @@ func executeAction(action mig.Action, prettyPrint bool) (cmd mig.Command, err er
 }
 
 // runModuleDirectly executes a module and displays the results on stdout
-func runModuleDirectly(mode string, args []byte, pretty bool) (out string) {
+func runModuleDirectly(mode string, args interface{}, pretty bool) (out string) {
 	if _, ok := modules.Available[mode]; !ok {
 		return fmt.Sprintf(`{"errors": ["module '%s' is not available"]}`, mode)
 	}
 	// instanciate and call module
 	run := modules.Available[mode].NewRun()
-	out = run.Run(os.Stdin)
+	msg, err := modules.MakeMessage(modules.MsgClassParameters, args)
+	if err != nil {
+		panic(err)
+	}
+	out = run.Run(bytes.NewBuffer(msg))
 	if pretty {
 		var modres modules.Result
 		err := json.Unmarshal([]byte(out), &modres)

.travis.yml
@@ -20,3 +20,4 @@ script:
     - cd "$GOPATH/src/mig.ninja/mig"
     - make
     - yes | bash tools/standalone_install.sh
+    - ./bin/linux/amd64/mig-agent-latest -i actions/example_v2.json

commit 15abdedc49fbaee9e5727472301f715999f84fbb

[minor/bug] fix module parameter input issues
This resolves a bug introduced in e2e687f. The args parameter to
runModuleDirectly() used to contain input parameters, but as of recent
changes to allow parameters via stdin does not.

Since the parameter was unused, this changes it to be an interface
pointer. If set, this will be used as module parameters, if not the
agent modules will expect input via stdin as normal.

mig-agent/agent.go
@@ -6,6 +6,7 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/json"
 	"flag"
@@ -131,12 +132,7 @@ func main() {
 			panic(err)
 		}
 	default:
-		var tmparg string
-		for _, arg := range flag.Args() {
-			tmparg = tmparg + arg
-		}
-		args := []byte(tmparg)
-		fmt.Printf("%s", runModuleDirectly(*mode, args, *pretty))
+		fmt.Printf("%s", runModuleDirectly(*mode, nil, *pretty))
 	}
 exit:
 }
@@ -191,17 +187,27 @@ func executeAction(action mig.Action, prettyPrint bool) (cmd mig.Command, err er
 }
 
 // runModuleDirectly executes a module and displays the results on stdout
-func runModuleDirectly(mode string, args interface{}, pretty bool) (out string) {
+//
+// paramargs allows the parameters to be specified as an argument to the
+// function, overriding the expectation parameters will be sent via
+// Stdin. If nil, the parameters will still be read on Stdin by the module.
+func runModuleDirectly(mode string, paramargs interface{}, pretty bool) (out string) {
 	if _, ok := modules.Available[mode]; !ok {
 		return fmt.Sprintf(`{"errors": ["module '%s' is not available"]}`, mode)
 	}
-	// instanciate and call module
-	run := modules.Available[mode].NewRun()
-	msg, err := modules.MakeMessage(modules.MsgClassParameters, args)
-	if err != nil {
-		panic(err)
+	infd := bufio.NewReader(os.Stdin)
+	// If parameters are being supplied as an argument, use these vs.
+	// expecting parameters to be supplied on Stdin.
+	if paramargs != nil {
+		msg, err := modules.MakeMessage(modules.MsgClassParameters, paramargs)
+		if err != nil {
+			panic(err)
+		}
+		infd = bufio.NewReader(bytes.NewBuffer(msg))
 	}
-	out = run.Run(bytes.NewBuffer(msg))
+	// instantiate and call module
+	run := modules.Available[mode].NewRun()
+	out = run.Run(infd)
 	if pretty {
 		var modres modules.Result
 		err := json.Unmarshal([]byte(out), &modres)