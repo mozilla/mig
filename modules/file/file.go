dfe3084824d8521e4f8075da2b2d39d2b2d31a9c
vladimirdiaconescu <gamemind@gmail.com>
2016-01-28 15:22:26 +0200

    Adding system calls with unknown origin to the sandbox
    System calls marked with "GO" signify that these are needed by the Go runtime, not by the module per se. The syscalls in question could originate from specific usecases, such as symlinks or /proc.

--- modules/file/file.go
+++ modules/file/file.go
@@ -73,8 +73,8 @@ func init() {
 					"stat",
 					"readlinkat",
 					"write",
-					"mmap",  // GO
-					"futex", // GO
+					"mmap",            // GO
+					"futex",           // GO
 					"getdents64",
 					"read",
 					"sigaltstack",     // GO
@@ -84,9 +84,11 @@ func init() {
 					"sched_yield",     // GO
 
 					// Used for pretty printing the violating syscall (rare)
-					"exit_group",   // Sandbox
-					"rt_sigreturn", // Sandbox
+					"exit_group",      // Sandbox
+					"rt_sigreturn",    // Sandbox
 
+					// System calls with unknown origin as of yet
+					"fsetxattr",
 				},
 				Action: sandbox.ActAllow,
 			},
