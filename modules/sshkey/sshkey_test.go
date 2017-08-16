53cfc5288b805d99b0fb10b375f2f96960e08c2f
Aaron Meihm <ameihm@mozilla.com>
2017-08-16 16:53:33 -0500

    add sshkey module
    sshkey scans the file system on remote agents for SSH keys, and where
    identified returns fingerprints for the keys.
    
    Where private keys and public keys are identified, fingerprints will be
    returned for these keys if possible.
    
    The module also identifies authorized_keys files, and returns the
    fingerprints of any public keys present in the authorized_keys file,
    allowing correlation between identified public/private fingerprints and
    the hosts the keys have access to.

---
+++ modules/sshkey/sshkey_test.go
@@ -0,0 +1,139 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Contributor: Aaron Meihm ameihm@mozilla.com [:alm]
+
+package sshkey /* import "mig.ninja/mig/modules/sshkey" */
+
+import (
+	"bytes"
+	"encoding/json"
+	"strings"
+	"testing"
+
+	"mig.ninja/mig/modules"
+	"mig.ninja/mig/testutil"
+)
+
+func TestRegistration(t *testing.T) {
+	testutil.CheckModuleRegistration(t, "sshkey")
+}
+
+var testAction = `
+{
+	"class": "parameters",
+	"parameters": {
+		"paths": [
+			"./testdata"
+		],
+		"maxdepth": 3
+	}
+}
+`
+
+func TestRun(t *testing.T) {
+	action := strings.Replace(testAction, "\n", "", -1)
+	reader := modules.NewModuleReader(bytes.NewReader([]byte(action)))
+	runner := run{}
+	res := runner.Run(reader)
+	result := modules.Result{}
+	err := json.Unmarshal([]byte(res), &result)
+	if err != nil {
+		t.Fatalf("json.Unmarshal: %v", err)
+	}
+	e := Elements{}
+	err = result.GetElements(&e)
+	if err != nil {
+		t.Fatalf("GetElements: %v", err)
+	}
+	if len(e.Keys) != 8 {
+		t.Fatalf("incorrect number of keys returned from module run")
+	}
+}
+
+func TestFindCandidates(t *testing.T) {
+	cands, err := findCandidates([]string{"./testdata"}, 2)
+	if err != nil {
+		t.Fatalf("findCandidates: %v", err)
+	}
+	if len(cands) != 7 {
+		t.Fatalf("findCandidates: incorrect number of candidates returned")
+	}
+
+	cands, err = findCandidates([]string{"/nonexist"}, 2)
+	if err != nil {
+		t.Fatalf("findCandidates: %v", err)
+	}
+	if len(cands) != 0 {
+		t.Fatalf("findCandidates: incorrect number of candidates returned")
+	}
+}
+
+func TestProcessCandidate(t *testing.T) {
+	e := &Elements{}
+	processCandidate("./testdata/home/testkey1", e)
+	if len(e.Keys) != 1 {
+		t.Fatalf("processCandidate: number of keys in elements not correct")
+	}
+	if e.Keys[0].Type != KeyTypePrivate {
+		t.Fatalf("processCandidate: invalid key type returned")
+	}
+	if e.Keys[0].FingerprintMD5 != "4c:57:cb:9d:3b:8e:eb:80:3f:d2:97:2e:ad:64:67:eb" {
+		t.Fatalf("processCandidate: invalid fingerprint")
+	}
+
+	e = &Elements{}
+	processCandidate("./testdata/testkey3.pub", e)
+	if len(e.Keys) != 1 {
+		t.Fatalf("processCandidate: number of keys in elements not correct")
+	}
+	if e.Keys[0].Type != KeyTypePublic {
+		t.Fatalf("processCandidate: invalid key type returned")
+	}
+	if e.Keys[0].FingerprintMD5 != "14:0f:ed:33:5d:27:57:58:2c:03:c8:3c:25:84:b0:ef" {
+		t.Fatalf("processCandidate: invalid fingerprint")
+	}
+
+	e = &Elements{}
+	processCandidate("./testdata/home/testkey2", e)
+	if len(e.Keys) != 1 {
+		t.Fatalf("processCandidate: number of keys in elements not correct")
+	}
+	if e.Keys[0].Type != KeyTypePrivate {
+		t.Fatalf("processCandidate: invalid key type returned")
+	}
+	if e.Keys[0].FingerprintMD5 != "" {
+		t.Fatalf("processCandidate: fingerprint was not unset as expected")
+	}
+
+	e = &Elements{}
+	processCandidate("./testdata/home/authorized_keys", e)
+	if len(e.Keys) != 2 {
+		t.Fatalf("processCandidate: number of keys in elements not correct")
+	}
+	if e.Keys[1].Type != KeyTypeAuthorizedKeys {
+		t.Fatalf("processCandidate: invalid key type returned")
+	}
+	if e.Keys[1].FingerprintMD5 != "c0:bd:f1:e2:42:1c:36:cc:f8:f0:8a:cb:9f:12:b8:ad" {
+		t.Fatalf("processCandidate: invalid fingerprint")
+	}
+
+	e = &Elements{}
+	processCandidate("./testdata/home/testkey2.pub", e)
+	if len(e.Keys) != 1 {
+		t.Fatalf("processCandidate: number of keys in elements not correct")
+	}
+	if e.Keys[0].Type != KeyTypePublic {
+		t.Fatalf("processCandidate: invalid key type returned")
+	}
+	if e.Keys[0].FingerprintMD5 != "c0:bd:f1:e2:42:1c:36:cc:f8:f0:8a:cb:9f:12:b8:ad" {
+		t.Fatalf("processCandidate: invalid fingerprint")
+	}
+
+	e = &Elements{}
+	processCandidate("./testdata/home/notakey", e)
+	if len(e.Keys) != 0 {
+		t.Fatalf("processCandidate: number of keys in elements not correct")
+	}
+}
