ca89819bed6adb07cb21321a0562e57b9b38078b
Zack Mullaly <zmullaly@mozilla.com>
2018-05-04 11:50:24 -0400

    Put together some placeholder test and implementation code for a new dispatch endpoint

---
+++ client/mig-client-daemon/api/actions/dispatch.go
@@ -0,0 +1,51 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Contributor: Zack Mullaly zmullaly@mozilla.com [:zack]
+
+package actionsAPI
+
+import (
+	"encoding/json"
+	"net/http"
+
+	//"github.com/gorilla/mux"
+
+	"mig.ninja/mig/client/mig-client-daemon/actions"
+	"mig.ninja/mig/client/mig-client-daemon/ident"
+)
+
+// dispatchRequest contains the parameters of a request to dispatch an action.
+type dispatchRequest struct {
+	ActionID ident.Identifier
+}
+
+// dispatchResponse contains the body of a response to a dispatch request.
+type dispatchResponse struct {
+	Error  *string `json:"error"`
+	Status string  `json:"status"`
+}
+
+// DispatchHandler is an HTTP handler for requests to have an action dispatched
+// to the MIG API.
+type DispatchHandler struct {
+	actionCatalog *actions.Catalog
+}
+
+// NewDispatchHandler
+func NewDispatchHandler(catalog *actions.Catalog) DispatchHandler {
+	return DispatchHandler{
+		actionCatalog: catalog,
+	}
+}
+
+func (handler DispatchHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	response := json.NewEncoder(res)
+
+	response.Encode(&dispatchResponse{
+		Error:  nil,
+		Status: "dispatched",
+	})
+}
