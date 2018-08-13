3bc501040dd7265ee10cc96723a3a6bc96742fe0
Jonathan Claudius <jclaudius@mozilla.com>
2018-08-13 19:05:49 -0400

    Propose install script to setup mig

---
+++ install.sh
@@ -0,0 +1,18 @@
+# ** DO BASIC GO ENV CHECKS AND SETUP IF NEEDED ** #
+
+# Check if Go is installed at a minimum version we care about
+# ADD CODE
+
+# Check if GOPATH is defined, if pre-existing prompt user to leave/overwrite
+# ADD CODE
+
+# Export path, prompt user to install exported PATH changes to ~/.bashrc ~/.bash_profile (so their mig env doesn't break when they close the terminal)
+# ADD CODE
+
+# ** Install MIG/MIG-console ** #
+
+# prompt to install mig, install if user says yes
+# ADD CODE
+
+# prompt to install mig-console, install if user says yes
+# ADD CODE
